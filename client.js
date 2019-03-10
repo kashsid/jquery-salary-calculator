a135a8ca51614f56d1a2ce6b474e20f7c0a62727
kashsid <kashifsiddique@gmail.com>
2019-03-10 18:06:24 -0500

    test the functionality. fixed monthly salary calculation.

--- client.js
+++ client.js
@@ -30,10 +30,10 @@ Function mae: newCar
 */
 function addEmployee(firstName, lastName, id, title, annualSalary) {
     console.log('in addEmployee:', firstName, lastName, id, title, annualSalary);
-    //let result = isValid(); // Calling isValid function and storing the result into variable "result"
+    ///let result = isValid(); // Calling isValid function and storing the result into variable "result"
 
-    //if (result == true) {
-    //console.log('validated');
+   /// if (result == true) {
+   
     let newEmployee = new Employee($('#firstName').val(),
         $('#lastName').val(),
         $('#id').val(),
@@ -44,25 +44,33 @@ function addEmployee(firstName, lastName, id, title, annualSalary) {
     console.log(employees);
 
     //disableControls();  // Calling function to disable controls
-    // emptyText();
+    clearForm();
+   
     displaySalaryTable();
-    //totalMonthly();
+   
     return true;
-    // }//end if
+    /// } //end if
 } // end newCard
+
 
 function displaySalaryTable() {
     console.log('in updateSalary Function');
-    let totalSalary = 0;
+    let totalMonthlySalary = 0;
     let outputTable = $('.employeeTable');
+   
     outputTable.empty();
+
     outputTable.append(`<tr><td><strong>First Name</strong></td>
                         <td><strong>Last Name</strong></td>
                         <td><strong>ID</strong></td>
                         <td><strong>Title</strong></td>
                         <td><strong>Annual Salary</strong>
-                        <td><strong>Remove Employee?</strong>
-                        </td></tr>`)
+                        <td><strong>Remove Employee?</strong></td>
+                        </tr>
+                        <h2 id="totalMonthly">Total Monthly: $<span id="monthlySalary">0.00</span></h2>`)
+                       
+
+                        
     for (newEmployee of employees) {
         outputTable.append(`<tr><td> ${newEmployee.firstName} </td> 
                             <td>${newEmployee.lastName}</td>
@@ -71,54 +79,91 @@ function displaySalaryTable() {
                             <td> $${Number(newEmployee.annualSalary).toFixed(2)} </td>
                             <td> <button class="removeEmployeeButton">Remove</button></td></tr>`);
 
-        totalSalary = totalSalary + (newEmployee.annualSalary / 12);
-        
+     totalMonthlySalary = totalMonthlySalary + (newEmployee.annualSalary / 12);
+        console.log(totalMonthlySalary);
+  //  }//end for of  
+     
+    
         $('#monthlySalary').empty();
-
-        $('#monthlySalary').append(totalSalary.toFixed(2));
-
-        if (totalSalary>= 20000){
+       
+        $('#monthlySalary').append(totalMonthlySalary.toFixed(2));
+     
+     
+        if (totalMonthlySalary >= 20000){
             console.log('this is 20K');
-            $('#monthlySalary').toggleClass('Salary'); 
-        } // check monthly salary
+            $('#monthlySalary').toggleClass('maxSalary'); 
+       } // end check toggleClass
+       
+     
+            
     } // end for of
-    console.log(totalSalary);
+    
 }// end displaySalaryTable
 
 function removeEmployee(){
+    let monthlySalaryElement = $('#monthlySalaryDiv');
     let currentRow = $(this).closest("tr"); 
     let col1 = currentRow.find("td:eq(0)").text(); // get current row 1st TD value
     let col2 = currentRow.find("td:eq(1)").text(); // get current row 2nd TD
     let col3 = currentRow.find("td:eq(2)").text(); // get current row 3rd TD
     
-    console.log(col1,col2,col3);
-    console.log(col1.trim(),col2.trim(),col3.trim());
-    
-    
-   // var data = col1 + "\n" + col2 + "\n" + col3;
+  
    for (let i=0; i < employees.length; i++){
        let fname=employees[i].firstName;
        let lname=employees[i].lastName;
-       let empId=employees[i].id;
-       ˇ
-       if (fname == col1.trim()){
-            employees.splice(i,1);
+       let empid=employees[i].id;
+       if (fname == col1.trim() && lname== col2.trim() && empid== col3.trim()){
+           $(this).parent().parent().fadeOut(600);
            
-            
-       }
+            employees.splice(i,1);
+
+           if (employees == undefined || employees.length == 0){
+                location.reload();
+            }
+           displaySalaryTable();
+          
+         
+            }
    }
 
-   // alert(data);
-   // console.log('current row is ',currentRow);
-    
-//var texto = $('table tr:nth-child',(currentRow),'td:nth-child(1)').text()
-    //var texto = $('.etable').find("tr:eq(2)").find("td:eq(1)").html();
-    //console.log(texto);
-    $(this).parent().parent().fadeOut(400);
    
-       
+   
+    } // end removeEmployee 
+    // Function to clear the text fields after the data is entered.
+    function clearForm(){
+        $('#firstName').val('');
+        $('#lastName').val('');
+        $('#id').val('');
+        $('#title').val('');
+        $('#annualSal').val('');
+    }// end clearForm
 
+    // function to chaeck if all fields are filled in.
 
-
+function isValid() {
+    console.log('in is empty');
+    if ($('#firstName').val() == '') {
+        alert("Please enter first name");
+        return false;
+    }
+    else if ($('#lastName').val() == '') {
+        alert("Please enter last name");
+        return false;
+    }
+    else if ($('#id').val() == '') {
+        alert("Please enter employee id");
+        return false;
+    }
+    else if ($('#title').val() == '') {
+        alert("Please enter employee title");
+        return false;
+    }
     
-}// end removeEmployee function
+     else if ($('#annualSal').val() == '') {
+        alert("Please enter employee annual salary");
+    return false;
+}
+    return true;
+}// End isValid
+
+
